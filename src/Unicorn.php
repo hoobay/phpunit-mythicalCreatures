40f4564c756af07cd306055d2904596cab96a90e
hoobay <hoobay69@gmail.com>
2026-02-18 10:53:07 +0100

    unicorn

--- src/Unicorn.php
+++ src/Unicorn.php
@@ -4,4 +4,34 @@ declare(strict_types=1);
 
 namespace MythicalCreatures;
 
+class Unicorn
+{
+    private string $name;
+    private string $color;
 
+    public function __construct(string $name, string $color = 'silver')
+    {
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function isSilver(): bool
+    {
+        return $this->color === 'silver';
+    }
+
+    public function say(string $message): string
+    {
+        return "**;* {$message} **;*";
+    }
+}
