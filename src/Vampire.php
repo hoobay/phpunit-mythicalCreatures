efcf793d1864f003fb706db87cab6fa72155d725
hoobay <hoobay69@gmail.com>
2026-02-18 11:21:40 +0100

    vampire

--- src/Vampire.php
+++ src/Vampire.php
@@ -4,4 +4,37 @@ declare(strict_types=1);
 
 namespace MythicalCreatures;
 
+class Vampire
+{
+    private string $name;
+    private string $pet;
+    private bool $thirsty;
+    
+    public function __construct(string $name = '', string $pet = 'bat', bool $thirsty = true)
+    {
+        $this->name = $name;
+        $this->pet = $pet;
+        $this->thirsty = $thirsty;
+    }
 
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPet(): string
+    {
+        return $this->pet;
+    }
+
+    public function isThirsty(): bool
+    {
+        return $this->thirsty;
+    }
+
+    public function drink(): bool
+    {
+        $this->thirsty = false;
+        return $this->thirsty;
+    }
+}
